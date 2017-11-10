038a735d8f6f0ad76824a5b882afab28233e0934
jamiemmt@cis.upenn.edu <truthslayer@users.noreply.github.com>
2017-11-10 15:31:01 -0500

    papers and teaching

---
+++ teaching/su-122/lectures/21-mem/stacks.h
@@ -0,0 +1,18 @@
+/* Interface for stacks
+ * 15-122 Principles of Imperative Computation, Fall 2010
+ * Frank Pfenning
+ */
+
+#include <stdbool.h>
+
+#ifndef _STACKS_H_
+#define _STACKS_H_
+
+typedef struct stack* stack;
+bool stack_empty(stack S);	/* O(1) */
+stack stack_new();		/* O(1) */
+void push(stack S, void* e);	/* O(1) */
+void* pop(stack S);		/* O(1) */
+void stack_free(stack S, void (*data_free)(void* e));	/* O(1) */
+
+#endif
